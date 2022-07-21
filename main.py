ee5642c6b3f2f3fbc4ce46e04a6ea40ff4c52d8e
AgamalARM <75130776+AgamalARM@users.noreply.github.com>
2022-07-21 09:34:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def root():
     reading_value =  y['sensor_treading_value']
     student_id = y['student_id']
     
-    ### Special Cases ###
+    ### Special Cases ###Rule==> there are {range & trend Flat & value within range}
     if (('range' in y) and (reading_value >= int(y['range']['from'])) and (reading_value <= int(y['range']['to'])) and (trend_name == 'Flat')):
         studentRange = y['range']
         return {"student_id": student_id,"Student_Range":studentRange,"value":reading_value,"trend":trend_name,"classification": 3,'alert':"non"}
